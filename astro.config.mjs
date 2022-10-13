1a3f27f020252f564dbba49146974768da309f4a
Sam Myers <sam.myers@protonmail.com>
2022-10-13 01:25:46 -0500

    feat: change domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://wedding.sammye.rs",
+  site: "https://samleahwedding.com",
   integrations: []
 });
